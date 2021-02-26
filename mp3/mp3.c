7d05de88cc1b066719fee63d4d25710bc91a4708
Manogna Rajanala <manogna3@illinois.edu>
2021-02-25 22:44:25 -0600

    mp3 updated

--- mp3/mp3.c
+++ mp3/mp3.c
@@ -1,14 +1,43 @@
+//Partners: smruthi2, manogna3
+//This program prints out a certain row in Pascal's triangle based on the user's input. 
+//It uses the data type, unsigned long and uses two for loops to calculate the Pascal's
+//triangle row and prints the row.
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-  int row;
+  int row; //declare row variable
 
-  printf("Enter a row index: ");
-  scanf("%d",&row);
+  printf("Enter a row index: "); //prompt user for row index
+  scanf("%d",&row); //read the input
 
-  // Write your code here
+//initialize variables for outer and inner loop and result
+
+    unsigned long n=0; 
+    unsigned long k=0;
+//first value of every row is 1
+    unsigned long result=1;
+   
+    
+    //outer loop that goes up to row 
+    for (n=0; n<=row; n++){
+        result=1;
+    //inner loop that goes up to n
+        for(k=1; k<=n; k++){
+            result=(result*(row+1-k))/(k); //calculates the value to be outputted based on row position
+           
+        }
+        printf("%lu ", result); //print final result
+    }
+
+
+
+
+
+ 
 
   return 0;
 }
+
+
